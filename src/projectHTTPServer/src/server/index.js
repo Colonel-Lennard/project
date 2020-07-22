7f276fc5889185a1aa45dd64bc82406a8448f9a6
Lennard Polierer <polierer@softbauware.de>
2020-07-22 14:52:39 +0200

    updated server client logic and div other things

--- src/projectHTTPServer/src/server/index.js
+++ src/projectHTTPServer/src/server/index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile('D:/Git/project/src/projectHTTPServer/src/client/index.html');
+    // res.sendFile('D:/Git/project/src/projectHTTPServer/src/client/main.html');
 });
+
+app.use(express.static('client'));
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
